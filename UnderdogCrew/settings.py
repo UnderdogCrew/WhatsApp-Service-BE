c722860b3c235fb2c5e2e0b5e20a766abf87f6ab
nilpatel <nilpatel62@gmail.com>
2024-11-30 10:13:39 +0530

    Added swagger for all the apis.

--- UnderdogCrew/settings.py
+++ UnderdogCrew/settings.py
@@ -58,6 +58,12 @@ INSTALLED_APPS = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Replace with your frontend URL
+    "https://whatsapp-api.theunderdogcrew.com/",
+    "*"
+]
+
 CORS_ALLOW_METHODS = (
     'DELETE',
     'GET',
