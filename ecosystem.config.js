e30e94bed90633f549150908f95f33ae45af8e84
nilpatel <nilpatel62@gmail.com>
2024-11-30 09:57:41 +0530

    Added api for validate the phone number and fetch the all the templates.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: 'whatsapp-service',
     script: 'manage.py',
-    args: 'runserver 0.0.0.0:8000',
+    args: 'runserver 0.0.0.0:8001',
     instances: 1,
     autorestart: true,
     watch: false,
