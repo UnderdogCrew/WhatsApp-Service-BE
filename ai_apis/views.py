d6ba53db50116ff01bed937fe3516c4b899d3ea0
nilpatel <nilpatel62@gmail.com>
2025-02-02 18:43:26 +0530

    Update datetime formatting in UserMessageLogs view
    Modify timestamp display format to remove microseconds and improve readability of created_at and updated_at timestamps

--- ai_apis/views.py
+++ ai_apis/views.py
@@ -796,12 +796,12 @@ class UserMessageLogs(APIView):
             message_list = []
             for _message in total_message:
                 # Convert ISO string to datetime object
-                human_readable = datetime.datetime.strftime(_message['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                human_readable = datetime.datetime.strftime(_message['created_at'], "%Y-%m-%d %H:%M:%S")
                 # Convert to a human-readable format
                 # human_readable = dt_obj.strftime("%Y-%m-%d %H:%M:%S")
                 
                 try:
-                    updated_at_human_readable = datetime.datetime.strftime(_message['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                    updated_at_human_readable = datetime.datetime.strftime(_message['updated_at'], "%Y-%m-%d %H:%M:%S")
                     # Convert to a human-readable format
                     # updated_at_human_readable = updated_dt_obj.strftime("%Y-%m-%d %H:%M:%S")
                 except:
