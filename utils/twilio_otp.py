b1f8e6b533c7f6db02e4e45bb1bcd5111b840887
Nayan Boghani <nayandeveloper63@gmail.com>
2025-02-28 21:11:14 +0530

    changed otp msg and timelimit

--- utils/twilio_otp.py
+++ utils/twilio_otp.py
@@ -1,7 +1,9 @@
 from twilio.rest import Client
 import random
 from UnderdogCrew import settings
-
+from datetime import datetime, timedelta
+import pytz
+from django.utils import timezone
 
 client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
@@ -15,8 +17,10 @@ def generate_otp():
 def send_otp(phone_number, otp):
     """Send OTP to the specified phone number using Twilio or a dummy value."""
     if settings.SEND_LIVE_OTP:
+        ist = pytz.timezone('Asia/Kolkata')
+        otp_expiry_time = timezone.now().astimezone(ist) + timedelta(minutes=5)
         message = client.messages.create(
-            body=f"Your OTP is {otp}",
+            body=f"{otp} is the OTP to verify your account with WapNeXus. OTP is valid till {otp_expiry_time.strftime('%H:%M:%S')} IST. do not share this OTP with anyone.",
             from_=settings.TWILIO_PHONE_NUMBER,
             to=phone_number
         )
