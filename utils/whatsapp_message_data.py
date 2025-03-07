19902e79da697892b8ac2b9b53b490dd975dd1dc
nilpatel <nilpatel62@gmail.com>
2025-03-07 21:15:17 +0530

    Handle optional message status in WhatsApp message sending
    - Add fallback to "sent" status if message status is not present in response
    - Improve robustness of message status tracking

--- utils/whatsapp_message_data.py
+++ utils/whatsapp_message_data.py
@@ -227,7 +227,7 @@ def send_message_data(number, template_name, text, image_url, user_id, entry=Non
                 "user_id": user_id,
                 "price": 0.125 if category == "UTILITY" else 0.875,
                 "id": response.json()['messages'][0]["id"],
-                "message_status": response.json()['messages'][0]["message_status"],
+                "message_status": response.json()['messages'][0]["message_status"] if "message_status" in response.json()['messages'][0] else "sent",
                 "created_at": datetime.datetime.now(),
                 "template_name": template_name
             }
