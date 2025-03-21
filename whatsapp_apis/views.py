76e0bc595bcc01d02bb209ecfb7a908a0e1d8c80
nilpatel <nilpatel62@gmail.com>
2025-03-21 11:42:55 +0530

    Update message status handling in CustomersChatLogs view
    - Extend the message status check to include 'failed' as a valid status, improving the accuracy of message type classification
    - Maintain existing functionality while enhancing the robustness of message processing logic

--- whatsapp_apis/views.py
+++ whatsapp_apis/views.py
@@ -582,7 +582,7 @@ class CustomersChatLogs(APIView):
 
             customer_chat_data = db.find_documents(collection_name="whatsapp_message_logs", query=query_filter, sort=sort_order)
             for _customer in customer_chat_data:
-                if _customer['message_status'] in ['read', 'delivered', 'sent', 'error']:
+                if _customer['message_status'] in ['read', 'delivered', 'sent', 'error', 'failed']:
                     msg_type = 1
                 else:
                     msg_type = 2
