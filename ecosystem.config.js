3e0e7983e5a3a079706cb3cc6922f43a61cf1722
Nayan Boghani <nayandeveloper63@gmail.com>
2026-01-28 19:47:29 +0530

    change export config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [{
     name: 'whatsapp-service',
-    script: 'newrelic-admin',
-    args: 'run-program python manage.py runserver 0.0.0.0:8001',
+    script: '/opt/python_apis/enve/bin/python',
+    args: 'manage.py runserver 0.0.0.0:8001',
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    max_memory_restart: '1G'
   }]
 };
