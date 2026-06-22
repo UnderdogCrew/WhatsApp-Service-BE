0aa6c1e7a02530234a05f1694f335605ad695b55
nilpatel <nilpatel62@gmail.com>
2026-06-22 19:34:45 +0530

    Update token validation in auth.py to allow 'webhook_api_key' as a valid token type alongside 'access', enhancing security checks for token usage.

--- utils/auth.py
+++ utils/auth.py
@@ -46,7 +46,7 @@ def token_required(f):
         try:
             data = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
             print(f"data: {data}")
-            if data.get('type') != 'access':
+            if data.get('type') != 'access' or data.get('type') != 'webhook_api_key':
                 raise jwt.InvalidTokenError('Invalid token type')
             current_user_id = data['user_id']
             current_user_email = data['user_email']
